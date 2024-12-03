a2a3788382547b60c78f727025454ddc91123a0b
RISHII7 <rishikesh.palande07@gmail.com>
2024-12-03 12:20:44 +0530

    Tiptap extensions

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,7 +1,20 @@
 "use client"
 
-import StarterKit from '@tiptap/starter-kit'
-import { useEditor, EditorContent } from '@tiptap/react'
+import { useEditor, EditorContent } from '@tiptap/react';
+
+import StarterKit from '@tiptap/starter-kit';
+
+import Image from '@tiptap/extension-image';
+import ImageResize from 'tiptap-extension-resize-image';
+
+import Table from '@tiptap/extension-table';
+import TableRow from '@tiptap/extension-table-row'
+import TableCell from '@tiptap/extension-table-cell';
+import TableHeader from '@tiptap/extension-table-header';
+
+import TaskItem from '@tiptap/extension-task-item';
+import TaskList from '@tiptap/extension-task-list';
+
 
 export const Editor = () => {
     const editor = useEditor({
@@ -11,7 +24,17 @@ export const Editor = () => {
                 class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text"
             }
         },
-        extensions: [StarterKit],
+        extensions: [
+            StarterKit,
+            Image,
+            ImageResize,
+            Table,
+            TableCell,
+            TableHeader,
+            TableRow,
+            TaskItem.configure({ nested: true }),
+            TaskList
+        ],
         content: '<p>Hello World! 🌎️</p>',
     });
     return (
