2174e752dd8cb9c48afde47231dbb9a5620d2aef
RISHII7 <rishikesh.palande07@gmail.com>
2024-12-10 11:28:43 +0530

    Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,17 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from 'next/font/google';
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const inter = Inter({
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
   title: "Docs",
   description: "A real time collaboration workspace",
+  icons: {
+    icon: "/logo.svg"
+  }
 };
 
 export default function RootLayout({
@@ -26,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={inter.className}
       >
         {children}
       </body>
