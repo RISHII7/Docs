fd3e769bc638c5afcb41eb13b81f870e473f6dc0
RISHII7 <rishikesh.palande07@gmail.com>
2024-12-11 11:21:01 +0530

    Search documents

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,28 +3,34 @@
 import { usePaginatedQuery } from "convex/react";
 import { api } from "../../../convex/_generated/api";
 
+import { useSearchParam } from "@/hooks/use-search-param";
+
 import { Navbar } from "./navbar";
 import { DocumentsTable } from "./documents-table";
 import { TemplateGallery } from "./templates-gallery";
 
 const Home = () => {
-  
-  const { results, status, loadMore } = usePaginatedQuery(api.documents.get, {}, { initialNumItems: 5 });
+  const [search] = useSearchParam();
+  const { results, status, loadMore } = usePaginatedQuery(
+    api.documents.get,
+    { search },
+    { initialNumItems: 5 }
+  );
 
   return (
-      <div className="min-h-screen flex flex-col">
-        <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
-          <Navbar />
-        </div>
-        <div className="mt-16">
-          <TemplateGallery />
-          <DocumentsTable 
-            documents={results}
-            loadMore={loadMore}
-            status={status}
-          />
-        </div>
+    <div className="min-h-screen flex flex-col">
+      <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
+        <Navbar />
       </div>
+      <div className="mt-16">
+        <TemplateGallery />
+        <DocumentsTable
+          documents={results}
+          loadMore={loadMore}
+          status={status}
+        />
+      </div>
+    </div>
   );
 }
 
